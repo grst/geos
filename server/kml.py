ab72214dc598ec7d2450d676b050abc6ccab6845
Gregor Sturm <mail@gregor-sturm.de>
2016-10-10 13:24:26 +0200

    Create KMLBuilder classes.
    Move the functions generating KML to own classes to streamline the
    main server module.
    
    Add small fixes to the KML generation. Displaying and loading the tiles
    properly works now.

---
+++ server/kml.py
@@ -0,0 +1,131 @@
+from pykml.factory import KML_ElementMaker as KML
+from geometry import *
+from lxml import etree
+
+SERVER_HOST = "127.0.0.1:5000"
+
+
+def get_map_url(map, z, x, y):
+    return "http://{}/maps/{}/{}/{}/{}.kml".format(SERVER_HOST, map.id, z, x, y)
+
+
+class KMLBuilder:
+    MIME_TYPE = "application/vnd.google-earth.kml+xml"
+
+    def __init__(self):
+        self.kml_doc = KML.kml()
+
+    def get_kml(self):
+        return etree.tostring(self.kml_doc, pretty_print=True, xml_declaration=True)
+
+
+class KMLMaster(KMLBuilder):
+    def __init__(self, mapsources):
+        kml_elements = []
+        for map_s in mapsources:
+            kml_elements.append(
+                KML.NetworkLink(
+                    KML.name(map_s.name),
+                    KML.Link(
+                        KML.href(get_map_url(map_s, 0, 0, 0)),
+                        KML.viewRefreshMode("onRegion")
+                    )
+                )
+            )
+        self.kml_doc = KML.kml(*kml_elements)
+
+
+class KMLRegion(KMLBuilder):
+    QUADS =  ['ne', 'se', 'sw', 'nw']
+
+    def lat_lon_box(self, geo_bb):
+        return (
+            KML.north(geo_bb.max.lat),
+            KML.south(geo_bb.min.lat),
+            KML.east(geo_bb.max.lon),
+            KML.west(geo_bb.min.lon)
+        )
+
+    def lod(self, min_lod_pixels=128, max_lod_pixels=-1):
+        return KML.Lod(
+            KML.minLodPixels(min_lod_pixels),
+            KML.maxLodPixels(max_lod_pixels)
+        )
+
+    def kml_region(self):
+        return KML.Region(
+            self.lod(),
+            KML.LatLonAltBox(
+                *self.lat_lon_box(self.tile_coords.geographic_bounds())
+            )
+        )
+
+    def get_quad_coords(self, quad):
+        assert quad in self.QUADS
+
+        z = self.tile_coords.zoom + 1
+        x = self.tile_coords.x * 2
+        y = self.tile_coords.y * 2
+        if quad == "nw":
+            pass
+        elif quad == "ne":
+            x += 1
+        elif quad == "se":
+            x += 1
+            y += 1
+        elif quad == "sw":
+            y += 1
+
+        return TileCoordinate(z, x, y)
+
+    def network_link(self, tile_coords):
+        return KML.NetworkLink(
+            KML.name("NL_{}_{}_{}".format(tile_coords.zoom, tile_coords.x, tile_coords.y)),
+            self.kml_region(),
+            KML.Link(
+                KML.href(get_map_url(self.mapsource, tile_coords.zoom, tile_coords.x, tile_coords.y)),
+                KML.viewRefreshMode("onRegion")
+            )
+        )
+
+    def make_child_links(self):
+        nls = []
+        for quad in  self.QUADS:
+            quad_coords = self.get_quad_coords(quad)
+            nls.append(self.network_link(quad_coords))
+        return nls
+
+    def ground_overlay(self):
+        z = self.tile_coords.zoom
+        x = self.tile_coords.x
+        y = self.tile_coords.y
+        return KML.GroundOverlay(
+            KML.name("GO_{}_{}_{}".format(z, x, y)),
+            KML.drawOrder(self.tile_coords.zoom),
+            KML.Icon(
+                KML.href(self.mapsource.get_tile_url(z, x, y))
+            ),
+            KML.LatLonBox(
+                *self.lat_lon_box(self.tile_coords.geographic_bounds())
+            ),
+        )
+
+    def __init__(self, mapsource, z, x, y):
+        self.tile_coords = TileCoordinate(z, x, y)
+        self.mapsource = mapsource
+        bbox = self.tile_coords.geographic_bounds()
+
+        kml_elems = []
+        kml_elems.append(KML.name("doc_{}_{}_{}".format(z, x, y)))
+        kml_elems.extend(self.make_child_links())
+        kml_elems.append(self.ground_overlay())
+
+        self.kml_doc = KML.kml(
+            KML.Document(
+                *kml_elems
+            )
+        )
+        #
+        # if z > 7:
+        #     # temporary recursion limit.
+        #     self.kml_doc = KML.Document()
--- server/server.py
+++ server/server.py
@@ -2,16 +2,9 @@ from flask import Flask, Response
 import argparse
 import os
 import mapsource
-from pykml.factory import KML_ElementMaker as KML
-from lxml import etree
-from geometry import *
+from kml import KMLMaster, KMLRegion
 
 app = Flask(__name__)
-SERVER_HOST = "127.0.0.1:5000"
-KML_LOD = KML.Lod(
-    KML.minLodPixels(128),
-    KML.maxLodPixels(-1)
-)
 
 
 @app.route('/')
@@ -19,116 +12,21 @@ def hello_world():
     return 'Hello World!'
 
 
-def get_map_url(map, z, x, y):
-    return "http://{}/maps/{}/{}/{}/{}.kml".format(SERVER_HOST, map.id, z, x, y)
-
-
 @app.route("/kml-master.kml")
 def kml_master():
     maps = mapsource.load_maps('/home/sturm/repos/other/geoverlay/server/mapsources')
     map = maps['osm.xml']
-    kml_doc = KML.kml(
-        KML.NetworkLink(
-            KML.name(map.name),
-            KML.Link(
-                KML.href(get_map_url(map, 0, 0, 0)),
-                KML.viewRefreshMode("onRegion")
-            )
-        )
-    )
-    return Response(etree.tostring(kml_doc, pretty_print=True), mimetype="application/vnd.google-earth.kml+xml")
-
-
-def lat_lon_box(geo_bb):
-    return (
-        KML.north(geo_bb.max.lat),
-        KML.south(geo_bb.min.lat),
-        KML.east(geo_bb.max.lon),
-        KML.west(geo_bb.min.lon)
-    )
-
-
-def network_link(map, tile_coords, quad):
-    assert quad in ['ne', 'se', 'sw', 'nw']
-    # geo_center = geo_bb.center()
-    # # GeographicBB(min_lon, min_lat, max_lon, max_lat)
-    # if quad == "nw":
-    #     geo_bb_new = GeographicBB(geo_bb.min.lon, geo_bb.min.lat,
-    #                               geo_center.lon, geo_center.lat)
-    # elif quad == "ne":
-    #     geo_bb_new = GeographicBB(geo_center.lon, geo_bb.min.lat,
-    #                               geo_bb.max.lon, geo_center.lat)
-    # elif quad == "sw":
-    #     geo_bb_new = GeographicBB(geo_bb.min.lon, geo_center.lat,
-    #                               geo_bb.max.lat, geo_center.lon)
-    # elif quad == "se":
-    #     geo_bb_new = GeographicBB(geo_center.lon, geo_center.lat,
-    #                               geo_bb.max.lon, geo_bb.max.lat)
-
-    z = tile_coords.zoom + 1
-    x = tile_coords.x
-    y = tile_coords.y
-    if quad == "nw":
-        pass
-    elif quad == "ne":
-        x += 1
-    elif quad == "se":
-        x += 1
-        y += 1
-    elif quad == "sw":
-        y += 1
-
-    new_tile_coords = TileCoordinate(z, x, y)
-
-    nl = KML.NetworkLink(
-        KML.name("{}_{}_{}_{}".format(quad, z, x, y)),
-        KML.Region(
-            KML_LOD,
-            *lat_lon_box(new_tile_coords.geographic_bounds())
-        ),
-        KML.Link(
-            KML.href(get_map_url(map, z, x, y)),
-            KML.viewRefreshMode("onRegion")
-        )
-    )
-    return nl
+    kml_doc = KMLMaster([map])
+    return Response(kml_doc.get_kml(), mimetype=kml_doc.MIME_TYPE)
 
 
 @app.route("/maps/<map_source>/<int:z>/<int:x>/<int:y>.kml")
 def kml_region(map_source, z, x, y):
     maps = mapsource.load_maps('/home/sturm/repos/other/geoverlay/server/mapsources')
-    tile_coords = TileCoordinate(z, x, y)
-    bbox = tile_coords.geographic_bounds()
-    map = maps['osm.xml']
-    kml_doc = KML.kml(
-        KML.Document(
-            KML.name("doc_{}_{}_{}".format(z, x, y)),
-            KML.Region(
-                KML_LOD,
-                KML.LatLonAltBox(
-                    *lat_lon_box(bbox)
-                )
-            ),
-            network_link(map, tile_coords, "ne"),
-            network_link(map, tile_coords, "se"),
-            network_link(map, tile_coords, "sw"),
-            network_link(map, tile_coords, "nw"),
-            KML.GroundOverlay(
-                KML.name("ge_{}_{}_{}".format(z, x, y)),
-                KML.drawOrder(z),
-                KML.Icon(
-                    KML.href(map.get_tile_url(z, x, y))
-                ),
-                KML.LatLonBox(
-                    *lat_lon_box(bbox)
-                ),
-            )
-        )
-    )
-    # if z > 3:
-    #     # temporary recursion limit.
-    #     kml_doc = KML.Document()
-    return Response(etree.tostring(kml_doc, pretty_print=True), mimetype="application/vnd.google-earth.kml+xml")
+    map = maps[map_source]
+    kml_doc = KMLRegion(map, z, x, y)
+
+    return Response(kml_doc.get_kml(), mimetype=kml_doc.MIME_TYPE)
 
 
 if __name__ == '__main__':
