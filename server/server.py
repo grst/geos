55a0a1aba965128bd06819b64adaa17217b5ecf3
Gregor Sturm <mail@gregor-sturm.de>
2016-10-29 20:06:35 +0200

    Create command line options for host and port

--- server/server.py
+++ server/server.py
@@ -1,9 +1,13 @@
-from flask import Flask, Response, render_template, g
+#!/usr/bin/env python3
+
+from flask import Flask, Response, render_template
 import argparse
 import mapsource
 import pkg_resources
 from kml import *
 
+# todo pip
+# todo fix maps
 
 app = Flask(__name__)
 
@@ -47,19 +51,48 @@ def kml_region(map_source, z, x, y):
     return kml_response(kml_doc)
 
 
-def run_app():
-    argp = argparse.ArgumentParser("KML Overlay generator. ")
+def run_app(default_host="127.0.0.1", default_port=5000):
+    argp = argparse.ArgumentParser("GEOS. The Google Earth Overlay Server. ")
     argp.add_argument("-m", "--mapsource", required=False,
                       default=pkg_resources.resource_filename(__name__, "mapsources"),
                       help="path to the directory containing the mapsource files.")
+    argp.add_argument("-H", "--host", required=False,
+                      help="Hostname of the Flask app [default {}]".format(default_host),
+                      default=default_host)
+    argp.add_argument("-P", "--port", required=False,
+                      help="Port for the Flask app [default {}]".format(default_port),
+                      default=default_port)
+
+    # Two options useful for debugging purposes, but
+    # a bit dangerous so not exposed in the help message.
+    argp.add_argument("-d", "--debug", required=False,
+                      action="store_true", dest="debug", help=argparse.SUPPRESS)
+    argp.add_argument("-p", "--profile", required=False,
+                      action="store_true", dest="profile", help=argparse.SUPPRESS)
+
     args = argp.parse_args()
+
+    # If the user selects the profiling option, then we need
+    # to do a little extra setup
+    if args.profile:
+        from werkzeug.contrib.profiler import ProfilerMiddleware
+
+        app.config['PROFILE'] = True
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
+                                          restrictions=[30])
+        args.debug = True
 
     app.config.from_object('default_settings')
     app.config['url_formatter'] = URLFormatter(app.config["SERVER_NAME"],
                                                app.config["PREFERRED_URL_SCHEME"])
     app.config['mapsources'] = mapsource.load_maps(args.mapsource)
-    app.run()
+
+    app.run(
+        debug=args.debug,
+        host=args.host,
+        port=int(args.port)
+    )
+
 
 if __name__ == '__main__':
     run_app()
-
