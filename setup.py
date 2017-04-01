c1fb1d1c7c5403f2aa7ccd8d94e343ddd8419775
Gregor Sturm <mail@gregor-sturm.de>
2017-04-01 18:03:22 +0200

    Update version and upload to pypi

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import find_packages
 
 setup(
     name='geos',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
-    description='Server for displaying tiled maps in Google Earth',
+    description='Map server to view, measure and print maps in a web browser'
+                'and to display maps as an overlay in google earth.',
     author='Gregor Sturm',
     author_email='mail@gregor-sturm.de',
     url='https://github.com/grst/geos',  # use the URL to the github repo
-    keywords=['maps', 'google earth', 'overlay'],  # arbitrary keywords
+    keywords=['maps', 'google earth', 'overlay', 'map printing'],  # arbitrary keywords
     license='GPLv3',
     install_requires=[
         'flask', 'lxml'
