cc22e7be99a3057a2a03a33e65637b81019fe27c
Paul Hofmann <p.h.o@web.de>
2017-04-04 18:48:09 +0200

    Add '--display-*' cli-args for formatted url modification
     (useful behind a reverse-proxy)

--- geos/scripts/runserver.py
+++ geos/scripts/runserver.py
@@ -26,10 +26,23 @@ def run_app(default_host=app.config['HOST'], default_port=app.config['PORT']):
     argp.add_argument("-P", "--port", required=False,
                       help="Port for the Flask app [default {}]".format(default_port),
                       default=default_port)
+    argp.add_argument("--display-host", required=False,
+                      help="Hostname used for self-referencing links [defaults to Flask hostname]",
+                      default=None)
+    argp.add_argument("--display-port", required=False,
+                      help="Port used for self-referencing links [defaults to Flask port]",
+                      default=None)
+    argp.add_argument("--display-scheme", required=False,
+                      help="URI-scheme used for self-referencing links [default {}]".format(app.config["PREFERRED_URL_SCHEME"]),
+                      default=None)
 
     args = argp.parse_args()
 
-    app.config['url_formatter'] = URLFormatter(args.host, args.port, app.config["PREFERRED_URL_SCHEME"])
+    app.config['url_formatter'] = URLFormatter(
+        host=args.display_host if args.display_host else args.host,
+        port=args.display_port if args.display_port else args.port,
+        url_scheme=args.display_scheme if args.display_scheme else app.config["PREFERRED_URL_SCHEME"]
+    )
     app.config['mapsources'] = geos.mapsource.load_maps(MAPSOURCES)
     if args.mapsource is not None:
         app.config["mapsources"].update(geos.mapsource.load_maps(args.mapsource))
